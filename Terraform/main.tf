2b8dc84d2aa186770720b0a3dd80b6479d392241
vinodrec2004 <46589770+vinodrec2004@users.noreply.github.com>
2023-12-06 01:01:02 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,10 +12,10 @@ terraform {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "0.0.7"
+  version = "~> 19.20.0"
 
   region               = "us-east-1"
-  cluster_version      = "19.20.0"
+  cluster_version      = "1.27"
   cluster_name         = "my_eks_cluster"
   instance_types       = ["t2.small"]
   vpc_cidr             = "10.0.0.0/16"
