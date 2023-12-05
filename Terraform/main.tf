b52722fbfbbf1acba28f769e4eea9ac758b39931
vinodrec2004 <46589770+vinodrec2004@users.noreply.github.com>
2023-12-06 01:25:45 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 module "eks" {
-  source  = "https://github.com/samgabrail/terraform-aws-eks"
+  source  = "https://github.com/samgabrail/terraform-aws-eks/tree/main"
 
   region               = "us-east-1"
   cluster_version      = "1.27"
