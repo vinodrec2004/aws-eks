6c6dc45fc3770e065a203465eab550832cf3f148
vinodrec2004 <46589770+vinodrec2004@users.noreply.github.com>
2023-12-07 18:57:29 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,7 +13,7 @@ module "eks" {
 
   region               = "eu-central-1"
   cluster_version      = "1.27"
-  cluster_name         = "my_eks_cluster"
+  cluster_name         = "idp"
   instance_types       = ["t2.small"]
   vpc_cidr             = "10.0.0.0/16"
   cluster_min_size     = 1
